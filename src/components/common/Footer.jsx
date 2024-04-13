9c1b737f6d4cb180a4d2769d893c48ce803728bd
mluchyb <122744366+mluchyb@users.noreply.github.com>
2024-04-13 19:39:34 -0300

    se agrega footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="py-3 text-center bg-dark text-white">
+        <p>&copy;Todos los derechos reservados.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
