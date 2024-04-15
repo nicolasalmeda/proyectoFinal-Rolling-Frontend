403084c4bb91dd371a7947e8f09443d1e3bd94a4
Maximiliano Gomez <alexander34223@gmail.com>
2024-04-15 06:45:20 -0300

    card y paginacion de aminities estructura terminada con sus respectivas imagenes

--- src/components/aminitiesDetails/DetallesAminities.jsx
+++ src/components/aminitiesDetails/DetallesAminities.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { infoAminities } from '../../helpers/aminities';
-import {useParams} from 'react-router-dom'
-import './AmenitiesDetails.css'
-
+import { useParams } from 'react-router-dom';
+import { Carousel } from 'react-bootstrap';
+import './AmenitiesDetails.css'; // Asegúrate de tener los estilos de Bootstrap importados en tu proyecto
 
 const DetallesAminities = () => {
-  const {amenities}= useParams()
-
-    const detalles = infoAminities[amenities];
+  const { amenities } = useParams();
+  const detalles = infoAminities[amenities];
   
-    return (
-      <div className=' container text-center'>
-        <h2>{detalles.titulo}</h2>
-        <p>{detalles.descripcion}</p>
-        <div>
+  return (
+    <div className='text-center container-detailsAmenities'>
+      <h2>{detalles.titulo}</h2>
+      <p className='amenitiesDetails-p container'>{detalles.descripcion}</p>
+      <Carousel className='carousel-amenities'>
+        <Carousel.Item>
           <img className='img-amenities-1' src={detalles.img1} alt="" />
+        </Carousel.Item>
+        <Carousel.Item>
           <img className='img-amenities-1' src={detalles.img2} alt="" />
-        </div>
-      </div>
-    );
-  };
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='img-amenities-1' src={detalles.img3} alt="" />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
 
 export default DetallesAminities;
